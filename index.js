e5c8811060ffa6f52df094ad6a66cefd1c442e53
hasanMahmud743 <h.mahmud.7430@gmail.com>
2022-11-27 22:26:28 +0600

    buyer api delete add

--- index.js
+++ index.js
@@ -6,121 +6,121 @@ const app = express();
 const port = process.env.PORT || 6500;
 require("dotenv").config();
 
-
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGO_URI;
 // console.log(uri)
- 
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-
 async function run() {
-const bookCategories = client.db('KnowledgeDB').collection('book-collection')
-const orderedList = client.db('KnowledgeDB').collection('order-collection')
-const userList = client.db('KnowledgeDB').collection('user-collection')
-const productList = client.db('KnowledgeDB').collection('product-collection')
-
-
-
+  const bookCategories = client.db("KnowledgeDB").collection("book-collection");
+  const orderedList = client.db("KnowledgeDB").collection("order-collection");
+  const userList = client.db("KnowledgeDB").collection("user-collection");
+  const productList = client.db("KnowledgeDB").collection("product-collection");
 
   try {
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      console.log(email);
+      const token = jwt.sign(email, "dsfukjrtwhiufhsdjfouaoiuf", {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    });
 
-    app.post('/jwt', async(req, res)=>{
-      const email = req.body
-      console.log(email)
-      const token =  jwt.sign(email, 'dsfukjrtwhiufhsdjfouaoiuf', {expiresIn: '7d'})
-      res.send({token})
-    })
+    app.get("/categories/:title", async (req, res) => {
+      const categories = req.params.title;
+      const cursor = { categories };
+      const result = await bookCategories.find(cursor).toArray();
+      res.send(result);
+    });
 
-    app.get('/categories/:title', async(req, res)=>{
-      const categories = req.params.title
-      const cursor = {categories}
-      const result = await bookCategories.find(cursor).toArray()
-      res.send(result)
-    })
+    app.post("/categories", async (req, res) => {
+      const order = req.body;
+      const result = await orderedList.insertOne(order);
+      res.send(result);
+    });
 
-    app.post('/categories', async(req, res)=>{
-      const order = req.body
-      const result = await orderedList.insertOne(order)
-      res.send(result)
+    app.post("/users", async (req, res) => {
+      const order = req.body;
+      const result = await userList.insertOne(order);
+      res.send(result);
+    });
 
-    })
+    app.get("/orders", async (req, res) => {
+      const cursor = {};
+      const result = await orderedList.find(cursor).toArray();
+      res.send(result);
+    });
 
-    app.post('/users', async(req, res)=>{
-      const order = req.body
-      const result = await userList.insertOne(order)
-      res.send(result)
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productList.insertOne(product);
+      res.send(result);
+    });
 
-    })
-
-    app.get('/orders', async(req, res)=>{
-      const cursor = {}
-      const result = await orderedList.find(cursor).toArray()
-      res.send(result)
-    })
-
-
-    app.post('/products', async(req, res)=>{
-      const product = req.body
-      const result = await productList.insertOne(product)
-      res.send(result)
-    })
-
-
-    app.get('/products', async(req, res)=>{
-      const email = req.query.email
-      let cursor = {}
-      if(email){
-        cursor = {email}
+    app.get("/products", async (req, res) => {
+      const email = req.query.email;
+      let cursor = {};
+      if (email) {
+        cursor = { email };
       }
-      const result = await productList.find(cursor).toArray()
-      res.send(result)
-    })
+      const result = await productList.find(cursor).toArray();
+      res.send(result);
+    });
 
-    app.get('/allUser', async(req, res)=>{
-      const email = req.query.email
-      let cursor = {email}
-      if(email){
-        cursor = {email}
+    app.get("/allUser", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      let cursor = {};
+      if (email) {
+        cursor = { email };
       }
-      
-      const result = await userList.findOne(cursor)
-      res.send(result)
-    })
 
-    app.get('/allSeller', async(req, res)=>{
-      const cursor = {accountType: 'seller'}
-      const result = await userList.find(cursor).toArray()
-      res.send(result)
-    })
+      const result = await userList.findOne(cursor);
+      res.send(result);
+    });
 
-    app.get('/allBuyer', async(req, res)=>{
-      const cursor = {accountType: 'normal'}
-      const result = await userList.find(cursor).toArray()
-      res.send(result)
-    })
+    app.get("/allSeller", async (req, res) => {
+      const cursor = { accountType: "seller" };
+      const result = await userList.find(cursor).toArray();
+      res.send(result);
+    });
 
-    app.delete('/products/:id', async(req, res)=>{
-      const id = req.params.id
-      const cursor = {_id:ObjectId(id)}
-      const result = await productList.deleteOne(cursor)
-      res.send(result)
-    })
-    
+    app.delete("/allSeller/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: ObjectId(id) };
+      const result = await userList.deleteOne(cursor)
+      res.send(result);
+    });
 
-  } 
-  
-  finally {
+    app.get("/allBuyer", async (req, res) => {
+      const cursor = { accountType: "normal" };
+      const result = await userList.find(cursor).toArray();
+      res.send(result);
+    });
 
+    app.delete("/allBuyer/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: ObjectId(id) };
+      const result = await userList.deleteOne(cursor)
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: ObjectId(id) };
+      const result = await productList.deleteOne(cursor);
+      res.send(result);
+    });
+  } finally {
   }
 }
 
